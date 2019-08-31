070a05639b62418ebfea9d5937db57a91be11a14
Bishop Bader <bflatbader@gmail.com>
2019-08-30 20:29:26 -0400

    fixed game reset code, working on styles

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@ function randomNumber (min, max) {
 }
 
 function checkWinLose (x, y) {
-    if (x === y) {
+    if (x == y) {
         return "win";
     } else if (x > y) {
         return "lose"
@@ -12,26 +12,40 @@ function checkWinLose (x, y) {
     }
 }
 
-// Set random number values for goal and each crystal
-var goalNumber = randomNumber(19,120);
-var crys1Val = randomNumber(1,12);
-var crys2Val = randomNumber(1,12);
-var crys3Val = randomNumber(1,12);
-var crys4Val = randomNumber(1,12);
-console.log("Goal: " + goalNumber);
-console.log("Crystal 1: " + crys1Val);
-console.log("Crystal 2: " + crys2Val);
-console.log("Crystal 3: " + crys3Val);
-console.log("Crystal 4: " + crys4Val);
+function gameStart () {
+    // Set random number values for goal and each crystal
+    var goalNumber = randomNumber(19,120);
+    var crys1Val = randomNumber(1,12);
+    var crys2Val = randomNumber(1,12);
+    var crys3Val = randomNumber(1,12);
+    var crys4Val = randomNumber(1,12);
+    var score = 0;
 
-// Initialize variables
+    // Log values
+    console.log("Goal: " + goalNumber);
+    console.log("Crystal 1: " + crys1Val);
+    console.log("Crystal 2: " + crys2Val);
+    console.log("Crystal 3: " + crys3Val);
+    console.log("Crystal 4: " + crys4Val);
+
+    // Write values to document
+    $('#goal').text(goalNumber);
+    $('#score').text(score);
+
+    return { goalNumber : goalNumber, crys1Val : crys1Val, crys2Val : crys2Val, crys3Val : crys3Val, crys4Val : crys4Val, score : score };
+}
+
+data = gameStart();
+var goalNumber = data.goalNumber;
+var crys1Val = data.crys1Val;
+var crys2Val = data.crys2Val;
+var crys3Val = data.crys3Val;
+var crys4Val = data.crys4Val;
+var score = data.score;
+
+// Initialize variables and write to document
 var wins = 0;
 var losses = 0;
-var score = 0;
-
-// Write values
-$('#goal').text(goalNumber);
-$('#score').text(score);
 $('#wins').text(wins);
 $('#losses').text(losses);
 
@@ -58,9 +72,25 @@ $(document).ready(function () {
         if (check == "win") {
             wins++;
             $('#wins').text(wins);
-        } else if (check == "loses") {
-            lose++;
+            // Reset game
+            data = gameStart();
+            goalNumber = data.goalNumber;
+            crys1Val = data.crys1Val;
+            crys2Val = data.crys2Val;
+            crys3Val = data.crys3Val;
+            crys4Val = data.crys4Val;
+            score = data.score;
+        } else if (check == "lose") {
+            losses++;
             $('#losses').text(losses);
+            // Reset game
+            data = gameStart();
+            goalNumber = data.goalNumber;
+            crys1Val = data.crys1Val;
+            crys2Val = data.crys2Val;
+            crys3Val = data.crys3Val;
+            crys4Val = data.crys4Val;
+            score = data.score;
         }
 
     });
